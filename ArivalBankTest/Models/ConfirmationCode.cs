7e86780a3666fc7b005597dc28bc2b0046b85f3a
Billy <bimganda@gmail.com>
2023-11-01 13:57:56 -0400

    add ConfirmationCode model class

---
+++ ArivalBankTest/Models/ConfirmationCode.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArivalBankTest.Models
+{
+    public class ConfirmationCode
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public string Code { get; set; } = string.Empty;
+        public DateTime ExpirationTime { get; set; }
+    }
+}
