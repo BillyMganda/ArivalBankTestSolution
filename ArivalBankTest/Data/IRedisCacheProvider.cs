6f59b1ec617dc1b654a0b03ad2b83e80c2664540
Billy <bimganda@gmail.com>
2023-11-01 16:00:30 -0400

    create IRedisCacheProvider interface class and its implement ion

---
+++ ArivalBankTest/Data/IRedisCacheProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace ArivalBankTest.Data
+{
+    public interface IRedisCacheProvider
+    {
+        Task<T> GetAsync<T>(string key);
+        Task SetAsync<T>(string key, T value, TimeSpan expiration);
+        Task RemoveAsync(string key);
+    }
+}
