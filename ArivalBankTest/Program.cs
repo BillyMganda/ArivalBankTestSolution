c000fb8293577254e3237d9e5437129f6f84ee06
Billy <bimganda@gmail.com>
2023-11-01 15:23:56 -0400

    inject serilog to dependency injection container

--- ArivalBankTest/Program.cs
+++ ArivalBankTest/Program.cs
@@ -1,6 +1,7 @@
 using ArivalBankTest.Data;
 using ArivalBankTest.Services;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +10,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SQL_DBConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SQL_DBConnection")!));
 
 builder.Services.AddScoped<IConfirmationCodeRepository, ConfirmationCodeRepository>();
+
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddSerilog();
+});
 
 
 var app = builder.Build();
