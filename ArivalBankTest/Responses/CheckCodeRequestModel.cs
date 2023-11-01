1126e8bdce5baf48761a40bafce4d8f65de839fb
Billy <bimganda@gmail.com>
2023-11-01 14:22:00 -0400

    create response model

--- ArivalBankTest/Responses/CheckCodeRequestModel.cs
+++ ArivalBankTest/Responses/CheckCodeRequestModel.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace ArivalBankTest.Requests
+namespace ArivalBankTest.Responses
 {
-    public class SendCodeRequestModel
+    public class CheckCodeRequestModel
     {
         [Required]
         [RegularExpression(@"^\+\d{10,15}$")]
         public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public string Code { get; set; } = string.Empty;
     }
 }
